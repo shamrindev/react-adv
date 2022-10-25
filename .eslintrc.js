287209abdece0877016e9a97ae94d44001f0d404
Vladislav Shamrin <chamav98@gmail.com>
2022-10-25 10:00:00 +0000

    chore(eslint): add i18next plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:i18next/recommended',
     'airbnb',
   ],
   parser: '@typescript-eslint/parser',
@@ -18,6 +19,7 @@ module.exports = {
   plugins: [
     'react',
     '@typescript-eslint',
+    'i18next',
   ],
   rules: {
     'no-shadow': 'off',
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -2,17 +2,22 @@ import { FC } from 'react';
 import classNames from 'classnames';
 import { Link } from 'react-router-dom';
 import { RoutesPaths } from 'shared/constants/routes';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string,
 }
 
-const Navbar: FC<NavbarProps> = ({ className }) => (
-  <nav className={classNames(cls.navbar, className)}>
-    <Link className={classNames(cls.link)} to={RoutesPaths.MAIN}>Главная</Link>
-    <Link className={classNames(cls.link)} to={RoutesPaths.ABOUT}>О компании</Link>
-  </nav>
-);
+const Navbar: FC<NavbarProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  return (
+    <nav className={classNames(cls.navbar, className)}>
+      <Link className={classNames(cls.link)} to={RoutesPaths.MAIN}>{t('Главная')}</Link>
+      <Link className={classNames(cls.link)} to={RoutesPaths.ABOUT}>{t('О нас')}</Link>
+    </nav>
+  );
+};
 
 export default Navbar;
