040f0d3345eba100066a25914558bab3634c410f
Vladislav Shamrin <chamav98@gmail.com>
2022-10-28 12:48:00 +0000

    chore(webpack): add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import webpack, { DefinePlugin, WebpackPluginInstance } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
@@ -18,6 +19,9 @@ function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
     }),
     new DefinePlugin({
       __IS_DEV__: isDev,
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
     }),
   ];
 
