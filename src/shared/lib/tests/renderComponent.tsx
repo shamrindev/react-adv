72bf217a0dcaa863d2f70b15713b4de87eadaf1b
Vladislav Shamrin <chamav98@gmail.com>
2022-11-20 09:00:00 +0000

    test: wrap component tests in BrowserRouter

--- src/shared/lib/tests/renderComponent.tsx
+++ src/shared/lib/tests/renderComponent.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
+import { BrowserRouter } from 'react-router-dom';
 import { Theme } from '@/shared/constants/themes';
 import { ThemeProvider } from '@/app/providers/ThemeProvider';
 import i18n from '@/shared/configs/i18n/i18nForTests';
@@ -20,11 +21,13 @@ const TestProvider = ({ children, options }: TextProviderProps) => {
   } = options;
 
   return (
-    <I18nextProvider i18n={i18n}>
-      <ThemeProvider initState={theme}>
-        {children}
-      </ThemeProvider>
-    </I18nextProvider>
+    <BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+        <ThemeProvider initState={theme}>
+          {children}
+        </ThemeProvider>
+      </I18nextProvider>
+    </BrowserRouter>
   );
 };
 
