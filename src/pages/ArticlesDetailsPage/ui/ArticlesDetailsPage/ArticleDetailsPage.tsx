708bcab9ba254b0e49f50351b533e4e1adbbeca6
Vladislav Shamrin <chamav98@gmail.com>
2023-04-05 14:57:00 +0000

    feat: add the article-rating feature

--- src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticleDetailsPage.tsx
@@ -16,32 +16,53 @@ import { articleDetailsPageReducer } from '../../model/slices'
 import cls from './ArticleDetailsPage.module.scss'
 import { ArticleDetailsPageHeader } from './ArticleDetailsPageHeader/ArticleDetailsPageHeader'
 import { ArticleDetailsComments } from '@/pages/ArticlesDetailsPage/ArticleDetailsComments/ArticleDetailsComments'
+import { ArticleRating } from '@/features/ArticleRating'
 
 interface ArticlesDetailsPageProps {
   className?: string
+  storybookId?: string
 }
 
 const reducers: ReducersList = {
   articleDetailsPage: articleDetailsPageReducer,
 }
 
-const ArticleDetailsPage = ({ className }: ArticlesDetailsPageProps) => {
+const ArticleDetailsPage = ({
+  className,
+  storybookId,
+}: ArticlesDetailsPageProps) => {
   const { t } = useTranslation('article')
   const { id } = useParams<{ id: string }>()
 
-  // if (!id) {
-  //   return (
-  //     <div className={classNames(cls.articledetailspage, {}, [className])}>
-  //       {t('Статья не найдена')}
-  //     </div>
-  //   )
-  // }
+  if (storybookId) {
+    return (
+      <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+        <Page className={classNames(cls.articledetailspage, {}, [className])}>
+          <ArticleDetailsPageHeader />
+          <ArticleDetails id={storybookId} className={cls.mb_30} />
+          <ArticleRating articleId={storybookId} className={cls.mb_30} />
+          <ArticleRecommendationsList className={cls.mb_30} />
+          <ArticleDetailsComments id={storybookId} />
+        </Page>
+      </DynamicModuleLoader>
+    )
+  }
+
+  if (!id) {
+    return (
+      <div className={classNames(cls.articledetailspage, {}, [className])}>
+        {t('Статья не найдена')}
+      </div>
+    )
+  }
+
   return (
     <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
       <Page className={classNames(cls.articledetailspage, {}, [className])}>
         <ArticleDetailsPageHeader />
-        <ArticleDetails id={id} />
-        <ArticleRecommendationsList />
+        <ArticleDetails id={id} className={cls.mb_30} />
+        <ArticleRating articleId={id} className={cls.mb_30} />
+        <ArticleRecommendationsList className={cls.mb_30} />
         <ArticleDetailsComments id={id} />
       </Page>
     </DynamicModuleLoader>
