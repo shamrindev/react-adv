48ee2dc74aba2a60e05d3feb94bb5ef713d85051
Vladislav Shamrin <chamav98@gmail.com>
2023-06-16 16:49:00 +0000

    feat(widgets): redesign Navbar (Reddit top bar)

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,73 +1,10 @@
-import { LoginModal } from '@/features/AuthByUserName'
-import { AvatarDropdown } from '@/features/AvatarDropdown'
-import { NotificationButton } from '@/features/NotificationButton'
-import { memo, useCallback, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { useSelector } from 'react-redux'
-import { classNames } from '@/shared/lib/classNames/classNames'
-import { AppLink, AppLinkTheme } from '@/shared/ui/AppLink'
-import { Button, ButtonTheme } from '@/shared/ui/Button'
-import { HStack } from '@/shared/ui/Stack'
-import { Text, TextTheme } from '@/shared/ui/Text'
-import { getUserAuthData } from '../../../entities/User'
-import cls from './Navbar.module.scss'
-import { getRouteArticleCreate } from '@/shared/const/router'
-interface NavbarProps {
-  className?: string
-}
+import { toggleFeatures } from '@/shared/lib/features'
+import { Navbar as NavbarDeprecated, NavbarProps } from './deprecated/Navbar'
+import { Navbar as NavbarRedesigned } from './redesigned/Navbar'
 
-export const Navbar = memo(({ className }: NavbarProps) => {
-  const { t } = useTranslation('translation')
-  const [isAuthModal, setIsAuthModal] = useState(false)
-  const authData = useSelector(getUserAuthData)
-
-  const onCloseModal = useCallback(() => {
-    setIsAuthModal(false)
-  }, [])
-  const onShowModal = useCallback(() => {
-    setIsAuthModal(true)
-  }, [])
-
-  if (authData) {
-    return (
-      <header className={classNames(cls.navbar, {}, [className])}>
-        <Text
-          className={cls.appName}
-          title={t('Articles App')}
-          theme={TextTheme.INVERTED}
-        />
-        <AppLink
-          to={getRouteArticleCreate()}
-          theme={AppLinkTheme.SECONDARY}
-          className={cls.createBtn}
-        >
-          {t('Создание статьи')}
-        </AppLink>
-
-        <HStack gap="16" className={cls.actions}>
-          <NotificationButton />
-          <AvatarDropdown />
-        </HStack>
-      </header>
-    )
-  }
-  return (
-    <header className={classNames(cls.navbar, {}, [className])}>
-      <Text
-        className={cls.appName}
-        title={t('Articles App')}
-        theme={TextTheme.INVERTED}
-      />
-      <Button
-        theme={ButtonTheme.CLEAR_INVERTED}
-        onClick={onShowModal}
-        className={cls.links}
-      >
-        {t('Войти')}
-      </Button>
-      {isAuthModal && (
-        <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
-      )}
-    </header>
-  )
-})
+export const Navbar = (props: NavbarProps) =>
+  toggleFeatures({
+    name: 'isAppRedesigned',
+    on: () => <NavbarRedesigned {...props} />,
+    off: () => <NavbarDeprecated {...props} />,
+  })
---
+++ src/widgets/Navbar/ui/redesigned/Navbar.tsx
@@ -0,0 +1,72 @@
+import { LoginModal } from '@/features/AuthByUserName'
+import { AvatarDropdown } from '@/features/AvatarDropdown'
+import { NotificationButton } from '@/features/NotificationButton'
+import { memo, useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { classNames } from '@/shared/lib/classNames/classNames'
+import { AppLink } from '@/shared/ui/AppLink'
+import { Button, ButtonTheme } from '@/shared/ui/Button'
+import { HStack } from '@/shared/ui/Stack'
+import { Icon } from '@/shared/ui/Icon'
+import { Input } from '@/shared/ui/Input'
+import { getUserAuthData } from '@/entities/User'
+import { getRouteArticleCreate } from '@/shared/const/router'
+import BurgerIcon from '@/shared/assets/icons/burger.svg'
+import SearchIcon from '@/shared/assets/icons/search.svg'
+import { NavbarProps } from '../deprecated/Navbar'
+import cls from './Navbar.module.scss'
+
+export const Navbar = memo(({ className }: NavbarProps) => {
+  const { t } = useTranslation('translation')
+  const [isAuthModal, setIsAuthModal] = useState(false)
+  const [search, setSearch] = useState('')
+  const authData = useSelector(getUserAuthData)
+
+  const onCloseModal = useCallback(() => {
+    setIsAuthModal(false)
+  }, [])
+  const onShowModal = useCallback(() => {
+    setIsAuthModal(true)
+  }, [])
+
+  return (
+    <header className={classNames(cls.navbar, {}, [className])}>
+      <HStack gap="8" align="center" className={cls.left}>
+        <Icon Svg={BurgerIcon} width={28} height={28} className={cls.burger} />
+        <span className={cls.logo}>artHub</span>
+      </HStack>
+
+      <div className={cls.searchWrapper}>
+        <Icon Svg={SearchIcon} width={18} height={18} className={cls.searchIcon} />
+        <Input
+          className={cls.search}
+          value={search}
+          onChange={setSearch}
+          placeholder={t('Поиск статей')}
+        />
+      </div>
+
+      {authData ? (
+        <HStack gap="16" align="center" className={cls.actions}>
+          <AppLink to={getRouteArticleCreate()}>
+            <Button theme={ButtonTheme.BACKGROUND_INVERTED} className={cls.createBtn}>
+              {t('Создать')}
+            </Button>
+          </AppLink>
+          <NotificationButton />
+          <AvatarDropdown />
+        </HStack>
+      ) : (
+        <HStack gap="16" align="center" className={cls.actions}>
+          <Button theme={ButtonTheme.BACKGROUND_INVERTED} onClick={onShowModal}>
+            {t('Войти')}
+          </Button>
+          {isAuthModal && (
+            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
+          )}
+        </HStack>
+      )}
+    </header>
+  )
+})
