edbdf8dddaa7c6215da0544e3962a5c491d0e2a9
Vladislav Shamrin <chamav98@gmail.com>
2023-08-01 13:19:00 +0000

    feat(article-edit): polished empty state for the unimplemented editor
    The Create/Edit article route rendered a bare, unstyled heading and looked
    broken. Replace it with an intentional centered empty state (icon, title,
    'editor in development' note, and a Back to list action that returns to
    /articles), so the Create entry point no longer dead-ends on a blank page.

--- src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage.tsx
@@ -1,8 +1,14 @@
 import { classNames } from '@/shared/lib/classNames/classNames'
 import cls from './ArticleEditPage.module.scss'
 import { useTranslation } from 'react-i18next'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { Page } from '@/widgets/Page/Page'
+import { VStack } from '@/shared/ui/Stack'
+import { Text } from '@/shared/ui/Text'
+import { Button } from '@/shared/ui/Button'
+import { Icon } from '@/shared/ui/Icon'
+import ArticleIcon from '@/shared/assets/icons/article-20-20.svg'
+import { getRouteArticles } from '@/shared/const/router'
 
 interface ArticleEditPageProps {
   className?: string
@@ -11,12 +17,23 @@ interface ArticleEditPageProps {
 const ArticleEditPage = ({ className }: ArticleEditPageProps) => {
   const { t } = useTranslation()
   const { id } = useParams<{ id: string }>()
+  const navigate = useNavigate()
   const isEdit = Boolean(id)
+
+  const title = isEdit
+    ? t('Редактирование статьи с  ID = ') + id
+    : t('Создание новой статьи')
+
   return (
     <Page className={classNames(cls.articleeditpage, {}, [className])}>
-      {isEdit
-        ? t('Редактирование статьи с  ID = ') + id
-        : t('Создание новой статьи')}
+      <VStack gap="16" align="center" max className={cls.empty}>
+        <Icon Svg={ArticleIcon} width={48} height={48} className={cls.icon} />
+        <Text title={title} />
+        <Text text={t('Редактор статей в разработке')} className={cls.muted} />
+        <Button onClick={() => navigate(getRouteArticles())}>
+          {t('Назад к списку')}
+        </Button>
+      </VStack>
     </Page>
   )
 }
