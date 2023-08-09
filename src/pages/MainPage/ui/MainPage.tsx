55066782530c8ddcea824104edf9fc02a7ccbfff
Vladislav Shamrin <chamav98@gmail.com>
2023-08-09 13:24:00 +0000

    feat(redesign): real landing page for the home route
    The redesigned home was a single welcome card floating in an empty viewport — a
    weak first impression. Replace it with a proper Reddit-style landing: a hero
    with a tagline and a 'Browse articles' CTA, a 'Popular communities' grid, and a
    right rail with an About card and a Demo-access card (admin/123, repo/storybook
    links) so a visitor can sign in and explore immediately.

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,11 +4,33 @@ import GitHubIcon from '@/shared/assets/icons/github-octocat.svg'
 import StorybookIcon from '@/shared/assets/icons/storybook-icon.svg'
 import { toggleFeatures } from '@/shared/lib/features'
 import { Card } from '@/shared/ui/Card'
-import { VStack } from '@/shared/ui/Stack'
+import { HStack, VStack } from '@/shared/ui/Stack'
 import { Text, TextSize } from '@/shared/ui/Text'
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button'
+import { AppLink } from '@/shared/ui/AppLink'
+import { StickyContentLayout } from '@/shared/layouts'
+import { getRouteArticles } from '@/shared/const/router'
 import { Page } from '@/widgets/Page/Page'
 import { useTranslation } from 'react-i18next'
 import cls from './MainPage.module.scss'
+
+const communities = [
+  {
+    name: 'IT',
+    color: 'var(--accent-up)',
+    desc: 'Программирование и разработка',
+  },
+  {
+    name: 'SCIENCE',
+    color: 'var(--accent-down)',
+    desc: 'Исследования и открытия',
+  },
+  {
+    name: 'ECONOMICS',
+    color: 'var(--accent-positive)',
+    desc: 'Рынки и финансы',
+  },
+]
 const MainPage = memo(() => {
   const { t } = useTranslation('main')
 
@@ -16,17 +38,96 @@ const MainPage = memo(() => {
     name: 'isAppRedesigned',
     on: () => (
       <Page data-testid="MainPage">
-        <VStack gap="16" max className={cls.redesigned}>
-          <Card className={cls.welcomeCard}>
-            <VStack gap="8">
+        <StickyContentLayout
+          content={
+            <VStack gap="16" max>
+              <div className={cls.hero}>
+                <Text
+                  title={t('Лента вашего IT-сообщества')}
+                  size={TextSize.L}
+                  className={cls.heroTitle}
+                />
+                <Text
+                  text={t(
+                    'Читайте, обсуждайте и голосуйте за статьи об IT, науке и не только'
+                  )}
+                  className={cls.heroSubtitle}
+                />
+                <AppLink to={getRouteArticles()}>
+                  <Button
+                    theme={ButtonTheme.BACKGROUND_INVERTED}
+                    size={ButtonSize.L}
+                  >
+                    {t('Перейти к статьям')}
+                  </Button>
+                </AppLink>
+              </div>
+
               <Text
-                title={t('Полезные статьи на тему IT и не только')}
-                size={TextSize.L}
+                title={t('Популярные сообщества')}
+                className={cls.sectionTitle}
               />
-              <Text text={t('Описание')} />
+              <div className={cls.communities}>
+                {communities.map((c) => (
+                  <AppLink
+                    key={c.name}
+                    to={getRouteArticles()}
+                    className={cls.communityCard}
+                  >
+                    <span
+                      className={cls.commDot}
+                      style={{ backgroundColor: c.color }}
+                    />
+                    <VStack gap="4">
+                      {/* eslint-disable-next-line i18next/no-literal-string */}
+                      <span className={cls.commName}>r/{c.name}</span>
+                      <Text text={t(c.desc)} className={cls.muted} />
+                    </VStack>
+                  </AppLink>
+                ))}
+              </div>
             </VStack>
-          </Card>
-        </VStack>
+          }
+          right={
+            <VStack gap="16" max>
+              <Card className={cls.railCard}>
+                <VStack gap="8">
+                  <Text title={t('О проекте')} />
+                  <Text text={t('Описание')} className={cls.muted} />
+                </VStack>
+              </Card>
+              <Card className={cls.railCard}>
+                <VStack gap="8">
+                  <Text title={t('Демо-доступ')} />
+                  <Text
+                    text={t(
+                      'admin / 123 — администратор · user / 123 — пользователь'
+                    )}
+                    className={cls.muted}
+                  />
+                  <HStack gap="16" className={cls.railLinks}>
+                    <a
+                      href="https://github.com/Ch4m4/react-adv"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="GitHub"
+                    >
+                      <GitHubIcon width={26} height={26} />
+                    </a>
+                    <a
+                      href="https://github.com/Ch4m4/react-adv"
+                      target="_blank"
+                      rel="noreferrer"
+                      aria-label="Storybook"
+                    >
+                      <StorybookIcon width={26} height={26} />
+                    </a>
+                  </HStack>
+                </VStack>
+              </Card>
+            </VStack>
+          }
+        />
       </Page>
     ),
     off: () => (
